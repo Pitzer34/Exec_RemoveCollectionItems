c6303aac57263c010423d8b282871d93f7c5e1e3
Pitzer34 <peter00880@gmail.com>
2022-10-24 20:01:20 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //在集合中移除多個項目
+            List<Product> Product = new List<Product>()
+            {
+            new Product(){Name="1",Price=1},
+            new Product(){Name="2",Price=2},
+            new Product(){Name="3",Price=3},
+
+            };
+
+            Product.Remove(Product[0]);
+           
+
+        }
+    }
+    class Product
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+
+}
